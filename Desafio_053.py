47044aae8b21384408389ea6225247b4b6daab19
Rodrigo Rosa <rodrigo.carlos.rosa@gmail.com>
2021-05-23 13:53:42 -0300

    Desafio_053

--- Desafio_053.py
+++ Desafio_053.py
@@ -3,9 +3,36 @@
     Crie um programa que leia uma frase
     qualquer e diga se ela é um palíndromo,
     desconsiderando os espaços.
-    APOS A SOPA
-    A SACADA DA CASA
-    A TORRE DA DERROTA
-    O LOBO AMA O BOLO
+    APOS A SOPA -- A SACADA DA CASA
+    A TORRE DA DERROTA -- O LOBO AMA O BOLO
 '''
-
+# Recebe a frase, elimina os espaços no começo e fim, converte em maiúscula.
+frase = input('Digite a frase: ').strip().upper()
+# FAz o fatiamento das strings da frase e armazena em palavras.
+palavras = frase.split()
+# Concatena as pelavas usando um espaço vazio entre elas ''.
+junto = ''.join(palavras)
+# Cria uma variável vazia chamada inverso
+inverso = ''
+# Mostra o que foi feito anteriormente como parte do entendimento. 
+# Não faz parte do programa oficial, está aí para aprendizagem.
+print(
+f'A Frase {frase}\n'
+f'A Palavra {palavras}\n'
+f'Elas Juntas {junto}\n'
+f'O LEN(tamanho) de Junto é {len(junto)}'
+)
+'''
+O range começa na ultima posição do Len -1
+Vai até o índice zero[0], por isso o segundo parâmetro é -1
+O passo do Range é -1, pois vai é decrescente. 
+'''
+for i in range(len(junto) -1, -1, -1):
+    inverso += junto[i]
+# Mostra o resultado da inversão da string.
+print(f'O inverso de {junto} é {inverso}.')
+# se a str em inverso for igual a str em junto, temos um palíndromo. 
+if inverso == junto:
+    print(f'Temos um Palíndromo!!')
+else:
+    print(f'Não temos um Palíndromo.')
---
+++ Desafio_056.py
@@ -0,0 +1,11 @@
+# Curso em vídeo - Desafio 056 - FOR
+'''
+    Desenvolva um programa que leia o 
+    nome, idade e sexo de 4 pessoas. 
+    No final do programa mostre, A média de idade do grupo,
+    Qual o nome do homem mais velho, Quantas mulheres têm 
+    menos de 20 anos. 
+'''
+
+for i in range(4):
+    
