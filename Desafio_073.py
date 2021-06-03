93b569a52b18f32448f14fc51aedd06bdc53bac5
Rodrigo Rosa <rodrigo.carlos.rosa@gmail.com>
2021-06-03 14:09:02 -0300

    Update Desafio_073.py

--- Desafio_073.py
+++ Desafio_073.py
@@ -22,17 +22,17 @@ for i in range(5):  # Loop no For será repetido 5 vezes.
         f'Em {i+1}º lugar está o time do {times[i]}'
     )  # Imprime o valor contido no índice dentro da Tupla chamada times.
 # 2.Mostra os 4 últimos colocados
-print(quebra)
+
 print('Os 4 últimos são:')
 i = 8  # Atribui 8 à variável i.
 while i <= 11:  # Enquanto i for menor ou igual a 11:
     print(times[i])  # Mostra o time que se encontra na posição i da Tupla.
     i += 1  # Acrescenta 1 ao valor que está em i.
 # 3.Mostra a lista em ordem alfabética.
-print(quebra)
+
 print("Os times em ordem alfabética são:\n", sorted(times))
 # 4.Mostra a posição de determinado time no campeonato.
-print(quebra)
+
 clube = input("Digite o time: ")
 if clube in times:
     print(f'O time da {clube} está na', (times.index(clube) + 1), 'ª posição.')
