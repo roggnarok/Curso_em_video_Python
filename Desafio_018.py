337287662e241a56a8a20ea0981a063c08dfab4d
Rodrigo Rosa <rodrigo.carlos.rosa@gmail.com>
2021-06-03 14:53:08 -0300

    Desafio_018_Angulo_SinCosTang
    Convert a angle to degrees from radians, after that shows the Sin, Cos and Tan

---
+++ Desafio_018.py
@@ -0,0 +1,16 @@
+# Desafio018 - Recebe o valor de um ângulo e mostra o seno, cosseno e tangente.
+import math
+# Recebe o valor do ângulo.
+angulo = float(input('Digite o seu ângulo favorito: '))
+ang_rad = math.radians(angulo)  # Converte o ângulo em Radianos.
+# Mostra o ângulo em Radianos.
+print(f'O valor do ângulo {angulo}° em radianos é: {ang_rad:.4f} rad.')
+# Calcula o Seno (O ângulo DEVE estar em Radianos)!
+seno = math.sin(ang_rad)
+#Calcula o Cosseno (O ângulo DEVE estar em Radianos)!
+cosseno = math.cos(ang_rad)
+# Calcula a Tangente (O ângulo DEVE estar em Radianos)!
+tang = math.tan(ang_rad)
+# Mostra o resultado.
+print(f'O Seno é {seno:.4f}, o Cosseno é {cosseno:.4f}'
+      f' e a Tangente é {tang:.4f}')
