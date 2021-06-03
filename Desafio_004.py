1eca6a19f962b410b4e38f324ccda710772e8264
Rodrigo Rosa <rodrigo.carlos.rosa@gmail.com>
2021-06-03 14:50:40 -0300

    Verifica_a_String
    Verifica o que foi digitado.
    Numérico, decimal, alfanumérico, maiúsculo, minúsculo.

---
+++ Desafio_004.py
@@ -0,0 +1,18 @@
+# Desafio004
+# Recebe uma entrada do usuário e informa se é número,
+# alfabético, alfanumérico, decimal, maiúscula, minúscula ou se só tem espaços.
+entrada = input(
+    "Verifica o que é aquilo que foi digitado.\
+                    \nDigite qualquer coisa: "
+)
+print(f'O tipo primitivo é {type(entrada)}')
+print(
+    f'Só tem espaços?\t\t{entrada.isspace()}\
+      \nÉ um número?\t\t{entrada.isnumeric()}\
+      \nÉ alfabético?\t\t{entrada.isalpha()}\
+      \nÉ alfanumérico?\t\t{entrada.isalnum()}\
+      \nÉ decimal?\t\t{entrada.isdecimal()}\
+      \nEstá em maiúsculo?\t{entrada.isupper()}\
+      \nEstá em minúsculo?\t{entrada.islower()}\
+      \nEstá capitalizada?\t{entrada.istitle()}'
+)
