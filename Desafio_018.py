b6277a46bf0a1e7d401ff28fbd3e13c41cb84377
Rodrigo Rosa <rodrigo.carlos.rosa@gmail.com>
2022-10-30 17:34:48 -0300

    Ajuste

--- Desafio_018.py
+++ Desafio_018.py
@@ -1,15 +1,14 @@
 # Desafio018 - Recebe o valor de um ângulo e mostra o seno, cosseno e tangente.
 import math
 angulo = float(input('Digite o seu ângulo favorito: '))# Recebe o valor do ângulo.
-ang_rad = math.radians(angulo)                         # Converte o ângulo em Radianos.
-# Mostra o ângulo em Radianos.
-print(f'O valor do ângulo {angulo}° em radianos é: {ang_rad:.4f} rad.')
-# Calcula o Seno (O ângulo DEVE estar em Radianos)!
-seno = math.sin(ang_rad)
-#Calcula o Cosseno (O ângulo DEVE estar em Radianos)!
-cosseno = math.cos(ang_rad)
-# Calcula a Tangente (O ângulo DEVE estar em Radianos)!
-tang = math.tan(ang_rad)
+ang_rad = math.radians(angulo)       # Converte o ângulo de graus para Radianos.
+print(f'O valor do ângulo {angulo}° em radianos é: {ang_rad:.4f} rad.') # Mostra o ângulo em Radianos.
+#Realiza o cálculo do Seno, Cosseno e Tangente
+seno = math.sin(ang_rad) # Calcula o Seno (O ângulo DEVE estar em Radianos)!
+cosseno = math.cos(ang_rad) #Calcula o Cosseno (O ângulo DEVE estar em Radianos)!
+tang = math.tan(ang_rad) # Calcula a Tangente (O ângulo DEVE estar em Radianos)!
 # Mostra o resultado.
-print(f'O Seno é {seno:.4f}, o Cosseno é {cosseno:.4f}'
-      f' e a Tangente é {tang:.4f}')
+print(f'O Seno é {seno:.3f}\
+      \nO Cosseno é {cosseno:.3f}\
+      \nA Tangente é {tang:.3f}')
+#This job was made by Rodrigo. https://github.com/roggnarok
