cd44836033c043bb462ee9f809f412c9fd14bb18
Rodrigo Rosa <rodrigo.carlos.rosa@gmail.com>
2022-10-30 17:20:40 -0300

    ajustes no math

--- Desafio_017.py
+++ Desafio_017.py
@@ -1,9 +1,8 @@
 #nº17 programa recebe o valor do cateto oposto e do cateto adjacente e calcula a hipotenusa.
     #Ver as funções do módulo math
-import math
 from math import hypot # --> Da biblioteca math impota a função hypot
 catetoAdjacente = float(input('Digite o valor do Cateto Adjacente: '))
 catetoOposto = float(input('Digite o valor do Cateto Oposto: '))
-hipotenusa = math.hypot(catetoAdjacente, catetoOposto) #Cálculo da hipotenusa usando a função HYPOT
+hipotenusa = hypot(catetoAdjacente, catetoOposto) #Cálculo da hipotenusa usando a função HYPOT
 print(f'O valor da hipotenusa é {hipotenusa}')
 #This job was made by Rodrigo. https://github.com/roggnarok
